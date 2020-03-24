33be8cd189a12380f0219156b743b8b4d38a324f
Adam Ahrens <adam.aahrens@gmail.com>
2020-03-23 21:27:24 -0500

    Trying to hook up sendgrid in dev

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Sendgrid
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => Rails.application.credentials.sendgrid(:api_key),
+  :domain => 'localhost:3000',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :tls => true,
+  :ssl => true,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
